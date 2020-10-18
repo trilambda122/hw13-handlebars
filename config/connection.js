ae3c64a8104ff016162a520a2236a803e56bcaba
trilambda122 <trilambda122>
2020-10-18 14:02:46 -0500

    updated connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ const mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql(mysql.createConnection(process.env.JAWSDB_URL));
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
     connection = mysql.createConnection({
