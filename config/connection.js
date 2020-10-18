5793bdaac6c2038e6f09a38cd86c91bfcd4e2406
trilambda122 <trilambda122>
2020-10-18 13:57:59 -0500

    updated connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,28 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Toomey6307!",
-    database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Toomey6307!",
+//     database: "burgers_db"
+// });
+let connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql(mysql.createConnection(process.env.JAWSDB_URL));
+} else {
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Toomey6307!",
+        database: "burgers_db"
+    });
+
+}
 // Make connection.
 connection.connect((err) => {
     if (err) {
